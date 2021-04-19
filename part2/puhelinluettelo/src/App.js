3804f38683d1a3065a9693706395b63d1563bf21
Henrik Vesterinen <henrikv@iki.fi>
2021-04-19 16:04:07 +0300

    part3: prepare for heroku and format code
    Signed-off-by: Henrik Vesterinen <henrikv@iki.fi>

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,54 +1,66 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import peopleService from "./services/people"
-import {Notification} from "./components/Notification"
-import {AddPeople, Filter, People} from "./components/People"
-
+import { Notification } from "./components/Notification"
+import { AddPeople, Filter, People } from "./components/People"
 
 const App = () => {
-  const [ people, setPeople] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filterText, setFilterText ] = useState('')
-  const [ message, setMessage ] = useState({})
+  const [people, setPeople] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [filterText, setFilterText] = useState("")
+  const [message, setMessage] = useState({})
 
   useEffect(() => {
-    peopleService
-      .getAll()
-      .then(response => setPeople(response.data))
+    peopleService.getAll().then((response) => setPeople(response.data))
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
-    let personExists = people.map(person => person.name).includes(newName)
+    let personExists = people.map((person) => person.name).includes(newName)
     if (personExists) {
-      let shouldUpdate = window.confirm(`${newName} already in DB, do you want to update the phone number?`)
+      let shouldUpdate = window.confirm(
+        `${newName} already in DB, do you want to update the phone number?`
+      )
       if (shouldUpdate) {
-        setPeople(people.map(person => person.name === newName? {...person, number: newNumber}: person))
+        setPeople(
+          people.map((person) =>
+            person.name === newName ? { ...person, number: newNumber } : person
+          )
+        )
         // Why is the personToUpdate still the old one???
         // It was just updated in the above line!
         // OK, setPeople is asynchronous and the result is not set immediately,
         // so the people variable still holds the old people list
         console.log(people)
-        let personToUpdate = people.find(person => person.name === newName)
+        let personToUpdate = people.find((person) => person.name === newName)
         console.log(personToUpdate)
         peopleService
-          .update(personToUpdate.id, {...personToUpdate, number: newNumber})
-          .then(response => console.log(response.data))
-          .then(() => notificator(`Successfully updated ${personToUpdate.name}`, "success"))
+          .update(personToUpdate.id, {
+            ...personToUpdate,
+            number: newNumber,
+          })
+          .then((response) => console.log(response.data))
+          .then(() =>
+            notificator(
+              `Successfully updated ${personToUpdate.name}`,
+              "success"
+            )
+          )
       }
-    
     } else {
       const newPerson = {
         name: newName,
-        number: newNumber
+        number: newNumber,
       }
       peopleService
         .create(newPerson)
-        .then(response => {
+        .then((response) => {
           console.log(`Adding person ${JSON.stringify(response.data)}`)
           setPeople(people.concat(response.data))
         })
-        .then(() => notificator(`Successfully added ${newPerson.name}`, "success"))
+        .then(() =>
+          notificator(`Successfully added ${newPerson.name}`, "success")
+        )
     }
     setNewName("")
     setNewNumber("")
@@ -57,40 +69,47 @@ const App = () => {
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleFilterUpdate = (event) => {
-    console.log(event.target.value);
+    console.log(event.target.value)
     setFilterText(event.target.value.toLowerCase())
   }
 
   const handleDelete = (event, personToDelete) => {
     // Is it useful/necessary to pass the event object here?
-    console.log(`Deleting now: ${JSON.stringify(personToDelete)}`);
+    console.log(`Deleting now: ${JSON.stringify(personToDelete)}`)
     if (window.confirm(`Delete ${personToDelete.name}?`)) {
       peopleService
-        .deleteOne(personToDelete.id) 
-        .then(setPeople(people.filter(person => person.id !== personToDelete.id)))
-        .then(() => notificator(`Successfully deleted ${personToDelete.name}`, "success"))
+        .deleteOne(personToDelete.id)
+        .then(
+          setPeople(people.filter((person) => person.id !== personToDelete.id))
+        )
+        .then(() =>
+          notificator(`Successfully deleted ${personToDelete.name}`, "success")
+        )
         .catch((err) => {
           console.log(err)
-          notificator(`Failed to delete ${personToDelete.name}, it was probably deleted from the server`, "error")
+          notificator(
+            `Failed to delete ${personToDelete.name}, it was probably deleted from the server`,
+            "error"
+          )
         })
     }
   }
 
   // Set the message back to null after 5s
   const notificator = (message, type) => {
-    let msg = {text: message, type: type}
+    let msg = { text: message, type: type }
     setMessage(msg)
     setTimeout(() => setMessage(null), 5000)
   }
 
-  const visiblePeople = filterText 
+  const visiblePeople = filterText
     ? people.filter((person) => person.name.toLowerCase().includes(filterText))
     : people
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <Notification message={message}/>
+      <Notification message={message} />
       <Filter value={filterText} onChange={handleFilterUpdate} />
       <AddPeople
         addPerson={addPerson}
@@ -99,9 +118,9 @@ const App = () => {
         handleNameChange={handleNameChange}
         handleNumberChange={handleNumberChange}
       />
-      <People people={visiblePeople} handleDelete={handleDelete}/>
+      <People people={visiblePeople} handleDelete={handleDelete} />
     </div>
-  );
+  )
 }
 
 export default App
--- part2/puhelinluettelo/src/services/people.js
+++ part2/puhelinluettelo/src/services/people.js
@@ -1,34 +1,34 @@
 import axios from "axios"
 
-const URL = "http://localhost:3001/people"
-
+// const URL = "http://localhost:3001/people"
+const URL = "/api/people" // when running in Heroku
 
 const getAll = () => {
-    return axios.get(URL)
+  return axios.get(URL)
 }
 
 const getOne = (id) => {
-    return axios.get(`${URL}/${id}`)
+  return axios.get(`${URL}/${id}`)
 }
 
 const create = (newPerson) => {
-    return axios.post(URL, newPerson)
+  return axios.post(URL, newPerson)
 }
 
 const update = (id, newPerson) => {
-    return axios.put(`${URL}/${id}`, newPerson)
+  return axios.put(`${URL}/${id}`, newPerson)
 }
 
 const deleteOne = (id) => {
-    return axios.delete(`${URL}/${id}`)
+  return axios.delete(`${URL}/${id}`)
 }
 
 const peopleService = {
-    getAll,
-    getOne,
-    create,
-    update,
-    deleteOne
+  getAll,
+  getOne,
+  create,
+  update,
+  deleteOne,
 }
 
 export default peopleService
